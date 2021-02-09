7e5cbd96d08355302a03329bcefa420d3b0affd8
secrettttt <243950548@qq.com>
2021-02-09 21:41:16 +0800

    feat: 支持pv、uv信息的上报到/emit/pv_uv_info接口

--- src/init.js
+++ src/init.js
@@ -1,8 +1,23 @@
-import { setPid, setUid } from './constant/index';
+import { setPid, setUid, getCurrentPid, getCurrentUid } from './constant/index';
+import reqwest from 'reqwest';
 
 const init = (obj) => {
   setPid(obj.pid);
   setUid(obj.uid);
+
+  // 页面初始化后，记录用户的访问记录
+  reqwest({
+    url: "http://101.200.197.197:8765/emit/pv_uv_info",
+    method: 'post',
+    type: 'json',
+    crossOrigin: true, /* 跨域请求 */
+    withCredentials: false, /* 值为false，表示前端向服务端发请求时不带cookie */
+    data: {
+      pid: getCurrentPid(),
+      uid: getCurrentUid(),
+      view_time: new Date().getTime()
+    }
+  });
 }
 
 export default init;
